bcbe4f7908aea556938e5e200155c5832c23a183
Don G <dgrote@refirmlabs.com>
2019-06-08 19:41:46 -0400

    started on /toggle endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request-promise');
+const {PIHOLE_URI} = process.env;
 
 router.patch('/toggle', (req, res) => {
-  res.sendStatus(501);
+  return request.get({uri: `${PIHOLE_URI}/admin/list.php`, json: true})
+    .then(response => {
+      res.sendStatus(501);
+    })
+    .catch(next);
 });
 
 router.get('/status', (req, res, next) => {
