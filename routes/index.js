79ac0ef100b6da162329a8b04e7a45a496073b0d
Don G <dgrote@refirmlabs.com>
2019-06-09 17:49:15 -0400

    Started interacting with running pi-hole
    /toggle fetches session cookie
    /status fetches domain blacklist

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,28 @@
-var express = require('express');
-var router = express.Router();
-var request = require('request-promise');
-const {PIHOLE_URI} = process.env;
+'use strict';
+const _ = require('lodash'),
+  router = require('express').Router(),
+  request = require('request-promise'),
+  {PIHOLE_URI} = process.env,
+  cookie = require('tough-cookie'),
+  log = require('debug-logger')('app:api');
 
-router.patch('/toggle', (req, res) => {
-  return request.get({uri: `${PIHOLE_URI}/admin/list.php`, json: true})
+router.patch('/toggle', (req, res, next) => {
+  return request.get({uri: `${PIHOLE_URI}/admin/list.php`, resolveWithFullResponse: true})
     .then(response => {
-      res.sendStatus(501);
+      log.debug('set-cookie', _.get(_.get(response, 'headers', {}), 'set-cookie'));
+      const cookies = _.get(response, 'headers.set-cookie', []).map(cookie.parse);
+      _.each(cookies, cook => log.debug('cookie', cook.toString()));
+      res.json(cookies);
     })
     .catch(next);
 });
 
 router.get('/status', (req, res, next) => {
-  res.sendStatus(501);
+  return request.get({uri: `${PIHOLE_URI}/admin/scripts/pi-hole/php/get.php?list=black`, json: true})
+    .then(response => {
+      const blacklist = _.flattenDeep(response);
+    })
+    .catch(next);
 });
 
 module.exports = router;
