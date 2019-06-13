967cb787a24ba565295390f69ad76ce52c61d17b
Don G <dgrote@refirmlabs.com>
2019-06-13 10:03:13 -0400

    forgot to add constants.js

---
+++ constants.js
@@ -0,0 +1,10 @@
+'use strict';
+
+exports = module.exports = {
+  PiHoleBlacklist: '/admin/list.php?l=black',
+  PiHoleAddBlacklist: '/admin/scripts/pi-hole/php/add.php',
+  PiHoleSubBlacklist: '/admin/scripts/pi-hole/php/sub.php',
+  PiHoleTokenOpenTag: '<div id="token" hidden>',
+  YouTubeContentDomain: 'googlevideo.com',
+  YouTubeContentDomainRegex: '(^|\\.)googlevideo\\.com$',
+};
