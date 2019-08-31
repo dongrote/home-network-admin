63bb9cc6378811f8c1cfcfb94e3583e79e72132f
Don Grote <don.grote@gmail.com>
2019-08-30 23:19:36 -0400

    wip

--- iptables/checkRuleExists.js
+++ iptables/checkRuleExists.js
@@ -4,6 +4,7 @@ const exec = require('./exec');
 exports = module.exports = rule => exec('-C', rule)
   .then(() => true)
   .catch(err => {
+    console.error(err);
     if (err.code === 1) {
       return true;
     }
