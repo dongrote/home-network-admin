2d262e8c4dfb1d07580a81d086d7964e6a704777
Don Grote <don.grote@gmail.com>
2020-02-24 09:34:10 -0500

    Implemented wake-on-lan
    Removed unnecessary public directory

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var _ = require('lodash');
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var log = require('debug-logger')('app');
@@ -13,7 +12,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use((req, res, next) => {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 'use strict';
 const router = require('express').Router(),
   iptables = require('./iptables'),
-  pihole = require('./pihole');
+  pihole = require('./pihole'),
+  wol = require('./wol');
 
 router.use('/iptables', iptables);
 router.use('/pihole', pihole);
+router.use('/wol', wol);
 
 exports = module.exports = router;
