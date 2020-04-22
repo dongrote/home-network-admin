3afb7dbb7fd5912d3b174317ede81248371dc261
Don Grote <don.grote@gmail.com>
2020-04-21 20:05:59 -0400

    Fixt: WakeUpButton was missing hwaddress

--- webui/src/WakeOnLanDevices.js
+++ webui/src/WakeOnLanDevices.js
@@ -6,6 +6,7 @@ export default props => (
   <LabeledButtonGroup color='yellow' label='Power'>
     {props.devices.map((dev, i) => <WakeOnLanDeviceRow
       key={i}
+      hwaddress={dev.hwaddress}
       device={dev.hostname}
       canonicalDevice={dev.name}
       onUnauthorized={props.onUnauthorized}
