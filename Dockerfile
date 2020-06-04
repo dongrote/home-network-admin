6308a3c081174a596435f27f04823b4ae7fbc135
Don Grote <don.grote@gmail.com>
2020-06-04 09:38:52 -0400

    Async radio button loading (#83)
    * Tweak AsyncRadioButton
    
    * Replace 'Working...' text with a spinner icon
    * Add an artificial 30s delay to WakeUpButton to give the target host a chance to start up
    * build multi-stage docker image

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,19 @@
-FROM node:12-alpine
+FROM node:12-alpine AS webui-builder
+WORKDIR /usr/src/app
+COPY webui .
+RUN npm i
+RUN npm run build
 
-RUN apk add openssl
+FROM node:12-alpine AS server-builder
 WORKDIR /usr/src/app
 COPY . .
 RUN rm -rf webui
+RUN npm i
+
+FROM node:12-alpine
+RUN apk add openssl
+WORKDIR /usr/src/app
+COPY --from=server-builder /usr/src/app/ .
+COPY --from=webui-builder /usr/src/app/build/ /usr/src/app/public
 RUN rm -f .env
 CMD npm start
--- webui/src/WakeUpButton.js
+++ webui/src/WakeUpButton.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import AsyncRadioButton from './AsyncRadioButton';
 
+const sleepTimeInSeconds = 30;
+const promiseSleep = s => new Promise(resolve => setTimeout(() => resolve(), s * 1000));
+
+const wakeUpTarget = mac => fetch(`/api/wol/?mac=${encodeURIComponent(mac)}`)
+  .then(res => res.status === 401 ? res : promiseSleep(sleepTimeInSeconds).then(() => res));
+
 export default props => (
   <AsyncRadioButton
-    icon
-    fluid
-    color='yellow'
     disabled={props.online}
-    onClick={props.online ? undefined : () => fetch(`/api/wol/?mac=${encodeURIComponent(props.mac)}`)
+    onClick={props.online ? undefined : () => wakeUpTarget(props.mac)
       .then(res => res.status === 401 ? props.onUnauthorized() : null)}
     checked={props.online}
     label={props.online ? 'On' : 'Off'}
