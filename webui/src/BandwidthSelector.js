45f356bc8d74a5132482fb440ab11f9f07f02852
Don Grote <don.grote@gmail.com>
2020-06-17 13:00:08 -0400

    wip

--- webui/src/BandwidthSelector.js
+++ webui/src/BandwidthSelector.js
@@ -1,33 +1,89 @@
 import React, { Component } from 'react';
-import { Button, Grid, Form, Icon } from 'semantic-ui-react';
+import { Dropdown, Button, Grid, Icon } from 'semantic-ui-react';
 
-const rangeToBandwidth = [
-  '28kbit',
-  '64kbit',
-  '128kbit',
-  '256kbit',
-  '512kbit',
-  '1mbit',
-  '2mbit',
-  '3mbit',
-  '4mbit',
-  '5mbit',
-  '8mbit',
-  '10mbit',
-  '16mbit',
-  '20mbit',
-  '25mbit',
-  '32mbit',
-  '64mbit',
-  '100mbit',
-  '128mbit',
-];
+const bandwidthOptions = [{
+  key: '28kbit',
+  value: '28kbit',
+  text: '28.8 kbps',
+}, {
+  key: '64kbit',
+  value: '64kbit',
+  text: '64 kbps',
+}, {
+  key: '128kbit',
+  value: '128kbit',
+  text: '128 kbps',
+}, {
+  key: '256kbit',
+  value: '256kbit',
+  text: '256 kbps',
+}, {
+  key: '512kbit',
+  value: '512kbit',
+  text: '512 kbps',
+}, {
+  key: '1mbit',
+  value: '1mbit',
+  text: '1 Mbps',
+}, {
+  key: '2mbit',
+  value: '2mbit',
+  text: '2 Mbps',
+}, {
+  key: '3mbit',
+  value: '3mbit',
+  text: '3 Mbps',
+}, {
+  key: '4mbit',
+  value: '4mbit',
+  text: '4 Mbps',
+}, {
+  key: '5mbit',
+  value: '5mbit',
+  text: '5 Mbps',
+}, {
+  key: '8mbit',
+  value: '8mbit',
+  text: '8 Mbps',
+}, {
+  key: '10mbit',
+  value: '10mbit',
+  text: '10 Mbps',
+}, {
+  key: '16mbit',
+  value: '16mbit',
+  text: '16 Mbps',
+}, {
+  key: '20mbit',
+  value: '20mbit',
+  text: '20 Mbps',
+}, {
+  key: '25mbit',
+  value: '25mbit',
+  text: '25 Mbps',
+}, {
+  key: '32mbit',
+  value: '32mbit',
+  text: '32 Mbps',
+}, {
+  key: '64mbit',
+  value: '64mbit',
+  text: '64 Mbps',
+}, {
+  key: '100mbit',
+  value: '100mbit',
+  text: '100 Mbps',
+}, {
+  key: '128mbit',
+  value: '128mbit',
+  text: '128 Mbps',
+}];
 
 class BandwidthSelector extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      bandwidthIndex: rangeToBandwidth.indexOf(props.bandwidth),
+      bandwidthIndex: bandwidthOptions.findIndex(opt => opt.value === props.bandwidth),
       loading: false,
       error: false,
     };
@@ -39,13 +95,13 @@ class BandwidthSelector extends Component {
 
   componentDidUpdate(prevProps) {
     if (this.props.bandwidth !== prevProps.bandwidth) {
-      this.setState({bandwidthIndex: rangeToBandwidth.indexOf(this.props.bandwidth)});
+      this.setState({bandwidthIndex: bandwidthOptions.findIndex(opt => opt.value === this.props.bandwidth)});
     }
   }
 
   async setBandwidth() {
     this.setState({loading: true});
-    var res = await fetch(`/api/throttle/bandwidth?bandwidth=${encodeURIComponent(rangeToBandwidth[this.state.bandwidthIndex])}`);
+    var res = await fetch(`/api/throttle/bandwidth?bandwidth=${encodeURIComponent(bandwidthOptions[this.state.bandwidthIndex].value)}`);
     this.setState({loading: false});
     if (!res.ok) {
       if (res.status === 401) {
@@ -61,19 +117,15 @@ class BandwidthSelector extends Component {
     return (
       <Grid columns={2}>
         <Grid.Row>
-          <Grid.Column textAlign='left'>
-            <Form>
-              <Form.Field>
-                <label>Bandwidth Limit {rangeToBandwidth[this.state.bandwidthIndex]}</label>
-                <input
-                  type='range'
-                  value={this.state.bandwidthIndex}
-                  min={0}
-                  max={rangeToBandwidth.length - 1}
-                  onChange={e => this.onBandwidthChange(e.target.value)}
-                />
-              </Form.Field>
-            </Form>
+          <Grid.Column>
+            <Dropdown
+              fluid
+              placeholder='Select a Bandwidth'
+              selection
+              value={bandwidthOptions[this.state.bandwidthIndex].value}
+              options={bandwidthOptions}
+              onChange={(e, data) => this.onBandwidthChange(bandwidthOptions.findIndex(opt => opt.value === data.value))}
+            />
           </Grid.Column>
           <Grid.Column textAlign='right'>
             <Button
--- webui/src/ThrottledDevices.js
+++ webui/src/ThrottledDevices.js
@@ -1,21 +1,54 @@
-import React from 'react';
-import { List, Label } from 'semantic-ui-react';
+import React, { Component } from 'react';
+import { Button, Grid } from 'semantic-ui-react';
 import LabeledButtonGroup from './LabeledButtonGroup';
 import BandwidthSelector from './BandwidthSelector';
+import AsyncButton from './AsyncButton';
+import AddThrottledHostForm from './AddThrottledHostForm';
 
-export default props => (
-  <LabeledButtonGroup color='teal' label='Throttle Control'>
-    <BandwidthSelector bandwidth={props.bandwidth} onUnauthorized={props.onUnauthorized}/>
-    {props.hosts.length && (
-      <List>
-        <List.Item>
-          <List.Header>Throttled Hosts</List.Header>
-        </List.Item>
-      {props.hosts.map((h, i) => (
-        <List.Item key={i}>{h}</List.Item>
-      ))}
-      </List>
-    )}
-    {props.hosts.length === 0 && <Label content='no throttled hosts'/>}
-  </LabeledButtonGroup>
-);
+const onRemoveHost = (h, onUnauthorized) => fetch(`/api/throttle/remove?hostname=${encodeURIComponent(h)}`)
+  .then(res => res.status === 401 ? onUnauthorized() : null);
+
+class ThrottledDevices extends Component {
+  state = {showAddNewHost: false};
+
+  async onRemoveHost(h) {
+    var res = await fetch(`/api/throttle/remove?hostname=${encodeURIComponent(h)}`);
+    if (res.status === 401) {
+      this.props.onUnauthorized();
+    }
+  }
+
+  render() {
+    return (
+      <LabeledButtonGroup color='teal' label='Throttle Control'>
+        <BandwidthSelector bandwidth={this.props.bandwidth} onUnauthorized={this.props.onUnauthorized}/>
+        <Grid>
+          {this.props.hosts.map((h, i) => (
+            <Grid.Row key={i} columns={2}>
+              <Grid.Column>
+                <Button fluid basic>{h}</Button>
+              </Grid.Column>
+              <Grid.Column>
+                <AsyncButton
+                  negative
+                  fluid
+                  content='Remove'
+                  onClick={() => this.onRemoveHost(h)}
+                />
+              </Grid.Column>
+            </Grid.Row>
+          ))}
+          <Grid.Row columns={1}>
+            <Grid.Column>
+              {this.state.showAddNewHost
+                ? <AddThrottledHostForm onUnauthorized={this.props.onUnauthorized} />
+                : <Button fluid content='Add New Host' onClick={() => this.setState({showAddNewHost: true})}/>}
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </LabeledButtonGroup>
+    );
+  }
+}
+
+export default ThrottledDevices;
