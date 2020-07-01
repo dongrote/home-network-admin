59df677cda55d625e7ed92c7942eeabb462c25b4
Don Grote <don.grote@gmail.com>
2020-07-01 06:33:44 -0400

    Created REST endpoint
    ```
    POST /api/auth/apitoken[?role={}]
    ```
    
    Returns an everlasting JWT with the `role` specified as a query parameter; default 'admin'.

---
+++ routes/auth/generateApiToken.js
@@ -0,0 +1,9 @@
+'use strict';
+const core = require('../../core');
+
+exports = module.exports = (req, res, next) => {
+  const role = _.get(req.query, 'role', 'admin');
+  return core.auth.generateApiToken(role)
+    .then(signedToken => res.json({token: signedToken}))
+    .catch(next);
+};
--- routes/auth/index.js
+++ routes/auth/index.js
@@ -4,8 +4,10 @@ exports = module.exports = router;
 const verify = require('./verify'),
   pubkey = require('./pubkey'),
   qrcode = require('./qrcode'),
+  generateApiToken = require('./generateApiToken'),
   requireAdmin = require('../../middleware/jwtVerify')('admin');
 
 router.get('/', verify);
+router.post('/apitoken', requireAdmin, generateApiToken);
 router.get('/pubkey', pubkey);
 router.get('/qrcode', requireAdmin, qrcode);
