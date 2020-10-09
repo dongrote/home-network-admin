3977ebcf478cd6da680983b62a93be116228cccc
Don Grote <don.grote@gmail.com>
2020-10-09 15:02:37 -0400

    adjust for alpine busybox df

--- core/system/storage.js
+++ core/system/storage.js
@@ -7,7 +7,7 @@ exports = module.exports = mountPoints => new Promise((resolve, reject) => {
   const mountPointArray = Array.isArray(mountPoints) ? mountPoints : [mountPoints],
     stats = [];
   let stdoutBuffer = Buffer.from('');
-  const child = cp.spawn('df', ['--output=target,size,used,avail'], {stdio: ['ignore', 'pipe', 'ignore']});
+  const child = cp.spawn('df', {stdio: ['ignore', 'pipe', 'ignore']});
   child.stdout
     .on('data', chunk => {
       stdoutBuffer = Buffer.concat([stdoutBuffer, chunk]);
@@ -18,13 +18,13 @@ exports = module.exports = mountPoints => new Promise((resolve, reject) => {
       let line;
       for (line of lines) {
         const stripped = line.replace(stripper, ' ');
-        const [mount, size, used, available] = stripped.split(' ');
+        const [unused, kblocks, used, available, cap, mount] = stripped.split(' ');
         if (_.includes(mountPointArray, mount)) {
           stats.push({
             mount,
-            size: Number(size),
-            used: Number(used),
-            available: Number(available)
+            size: Number(kblocks) * 1024,
+            used: Number(used) * 1024,
+            available: Number(available) * 1024,
           });
         }
       }
