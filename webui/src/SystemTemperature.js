7e1851b45c69df3c6f01fda4131a877b119791bc
Don Grote <don.grote@gmail.com>
2020-10-10 13:32:59 -0400

    restore f/c temp display

--- webui/src/SystemTemperature.js
+++ webui/src/SystemTemperature.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import {Chart} from 'react-google-charts';
 import { Grid } from 'semantic-ui-react';
 
+const ctof = c => Math.round((c * (9/5)) + 32);
+
 class SystemTemperature extends Component {
   state = {showFahrenheit: false};
 
@@ -9,19 +11,30 @@ class SystemTemperature extends Component {
     this.setState({showFahrenheit: !this.state.showFahrenheit, history: []});
   }
 
+  currentTemperature() {
+    return this.state.showFahrenheit ? `${this.props.fahrenheit}ºF` : `${this.props.celsius}ºC`;
+  }
+
+  adjustTemperatureValue(v) {
+    return this.state.showFahrenheit ? ctof(v) : v;
+  }
+
   render() {
     return (
-      <Grid.Row verticalAlign='middle'>
+      <Grid.Row verticalAlign='middle' onClick={() => this.setState({showFahrenheit: !this.state.showFahrenheit})}>
         <Grid.Column>
         <Chart
           chartType='AreaChart'
           loader={<div>Loading Data</div>}
-          data={[['Time', 'Temperature']].concat(this.props.history.map((t, i) => [i, t]))}
+          data={[['Time', 'Temperature']].concat(this.props.history.map((t, i) => [i, this.adjustTemperatureValue(t)]))}
           options={{
-            title: 'Temperature (C)',
+            title: `Current Temperature ${this.currentTemperature()}`,
             legend: {position: 'none'},
             hAxis: {textPosition: 'none'},
-            vAxis: {viewWindow: {max: 85, min: 20}},
+            vAxis: {viewWindow: {
+              max: this.adjustTemperatureValue(85),
+              min: this.adjustTemperatureValue(20),
+            }},
             colors: ['orange'],
           }}
         />
