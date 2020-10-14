af3fcba9bbf60af8c53a8a5876a31d6d2ce40f61
Don Grote <don.grote@gmail.com>
2020-10-14 15:35:44 -0400

    return 1/10th precision bandwidth usage values

--- core/throttle/usage.js
+++ core/throttle/usage.js
@@ -3,6 +3,8 @@ const env = require('../../env'),
   url = require('url'),
   rp = require('request-promise-native');
 
+const precisionRound = bps => Math.round((bps * 10)) / 10;
+
 exports = module.exports = (hostname, sampleTime) => {
   const queryParams = new url.URLSearchParams({hostname, sampleTime});
   return rp.get({
@@ -10,8 +12,8 @@ exports = module.exports = (hostname, sampleTime) => {
     uri: `${env.firewallApiUri()}/api/throttle/usage?${queryParams.toString()}`
   })
   .then(({bps}) => ({
-    Mbps: bps > 1000000 ? Math.round(bps / 1000000) : null,
-    Kbps: bps > 1000 ? Math.round(bps / 1000) : null,
-    bps: Math.round(bps),
+    Mbps: bps > 1000000 ? precisionRound(bps / 1000000) : null,
+    Kbps: bps > 1000 ? precisionRound(bps / 1000) : null,
+    bps: precisionRound(bps),
   }));
 };
